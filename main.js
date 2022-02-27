7fbbaa7f17f1a546d2a213e9f97cd098c5cc954d
21ciprian <2021ciprianoffice@gmail.com>
2022-02-27 15:10:38 +0000

    Declared global variables

--- main.js
+++ main.js
@@ -3,11 +3,14 @@ import englishToMorse from './letters.js'
 import morseToEnglish from './morseSymbols.js'
 const englishInput = document.querySelector('.english')
 const morseOutput = document.querySelector('.morse-output')
+const morseInput = document.querySelector('.morse')
+const englishOutput = document.querySelector('.english-output')
 console.log(facts.length)
 let carouselInner = document.querySelector('.carousel-inner')
 console.log('englishToMorse :', englishToMorse)
 console.log('morseToEnglish :', morseToEnglish)
 console.log('morseOutput :', morseOutput)
+console.log('englishOutput :', englishOutput)
 facts.map((mfact, i) => {
 	if (i === 0) {
 		const fact = document.createElement('p')
@@ -24,6 +27,8 @@ facts.map((mfact, i) => {
 })
 function englishTextConverter() {
 	console.log('englishTextConverter')
+	let convertedText = englishInput.value
+	convertedText = convertedText.toUpperCase().split('')
 }
 function morseTextConverter() {
 	console.log('morseTextConverter')
